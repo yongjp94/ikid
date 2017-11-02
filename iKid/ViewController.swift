9808759d638a7e146873d4a20b6b7650abe29749
yongjp94 <yongjun.park.94@gmail.com>
2017-11-02 13:45:39 -0700

    Initial Commit

---
+++ iKid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by Yongjun Park on 11/2/17.
+//  Copyright © 2017 Yongjun Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
